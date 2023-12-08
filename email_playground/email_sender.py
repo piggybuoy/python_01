1b489b8f0c1f4c0002c5309448eae7ce30df380f
piggybuoy <newnoodogs@hotmail.com>
2023-12-08 13:40:39 -0500

    email_playground_2023208 - improved email sender with html..

--- email_playground/email_sender.py
+++ email_playground/email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'tsmithdeveloper@hotmail.com'
 email['to'] = 'newnoodogs@hotmail.com'
 email['subject'] = 'You are something...'
 
-email.set_content('The tron is all mega up in here.')
+email.set_content(html.substitute({'name':'Megatron'}), 'html')
 
 with smtplib.SMTP(host='smtp.office365.com', port=587) as smtp:
     smtp.ehlo()
