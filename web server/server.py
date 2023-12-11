602b0a13a298729b8ef1f117c5e2820a24e4dcf8
piggybuoy <newnoodogs@hotmail.com>
2023-12-11 16:07:59 -0500

    portfo_20231211 - another commit.

--- web server/server.py
+++ web server/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -32,3 +34,34 @@ def work001():
 @app.route('/work2.html')
 def work002():
     return render_template('work2.html')
+
+@app.route('/thankyou.html')
+def thanks():
+    return render_template('thankyou.html')
+
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method =='POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'Did not save to db.'
+    else:
+        return 'problems...'
